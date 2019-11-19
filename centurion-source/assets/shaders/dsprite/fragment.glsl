1ac4714ceebf4206958c0545c346da2e8a0d6b46
fabian <fabio.marigo@outlook.it>
2019-11-20 00:21:34 +0100

    [Decorations] Removed antialiasing

--- centurion-source/assets/shaders/dsprite/fragment.glsl
+++ centurion-source/assets/shaders/dsprite/fragment.glsl
@@ -2,15 +2,15 @@
 
 in vec2 FragTex;
 
+out vec4 FragColor;
+
 uniform sampler2D texture1;
 uniform int not_placeable;
 
 void main()
 {
-    vec4 FragColor = texture(texture1, FragTex);
-    if (FragColor.a <= 0.8f) discard; 
+    FragColor = texture(texture1, FragTex);
+    if (FragColor.a <= 0.5f) discard; 
     
-	if (not_placeable == 1) gl_FragColor = vec4(FragColor.r, 0.f, 0.f, FragColor.a);
-
-    if (not_placeable == 0) gl_FragColor = FragColor;
+	if (not_placeable == 1) FragColor = vec4(FragColor.r, 0.f, 0.f, FragColor.a);
 }
