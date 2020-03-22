07fb6526ba51ec9d029076a6849af907d4f0bf78
Giorgio Oggiano <ilfolle64@gmail.com>
2020-03-22 17:37:37 +0100

    [GLOBAL] {Documented Utils folder}

--- centurion-source/source/util/file_manager.h
+++ centurion-source/source/util/file_manager.h
@@ -1,3 +1,11 @@
+/*
+* ---------------------------
+* CENTURION
+* [2019] - [2020] Rattlesmake
+* All Rights Reserved.
+* ---------------------------
+*/
+
 #ifndef UTILS_H
 #define UTILS_H
 
@@ -6,6 +14,9 @@
 
 using namespace std;
 
+/// <summary>
+/// This namespace contains main game folders constants.
+/// </summary>
 namespace Folders {
 
 	extern string XML_SCHEMAS;
@@ -17,7 +28,9 @@ namespace Folders {
 	extern string INTERFACE_EDITOR;
 };
 
-
+/// <summary>
+/// This namespace contains all methods and function for handling a file.
+/// </summary>
 namespace FileManager {
 
 	struct file_info {
@@ -25,28 +38,67 @@ namespace FileManager {
 		string path;
 	};
 
-	//
+	/// <summary>
+	/// This function reads and returns a text file content.
+	/// </summary>
+	/// <param name="fileLocation">Text file path. Supports only char* variables or strings converted to c_str.</param>
 	string ReadFile(const char* fileLocation);
 
-	// 
+	/// <summary>
+	/// This function returns all files within a specified folder path.
+	/// </summary>
+	/// <param name="folder">Folder path. Supports strings.</param>
+	/// <param name="type">The file extension required. Default value is "*", which means that every file extension it will be researched for.</param>
 	vector<string> GetAllFilesNamesWithinFolder(string folder, string type = "*");
 
-	// 
+	/// <summary>
+	/// This function returns all folders within a specified folder path.
+	/// </summary>
+	/// <param name="folder">Folder path. Supports strings.</param>
 	vector<string> GetAllFoldersNamesWithinFolder(string folder);
 
-	// 
+	/// <summary>
+	/// This function returns all files into all subfolders within a specified folder path.
+	/// </summary>
+	/// <param name="&folder_name">Folder path. Supports referenced strings.</param>
+	/// <param name="&file_extension">The file extension required. Default value is "", which means that every file extension it will be researched for.</param> 
 	vector<file_info> GetAllFilesNamesWithinSubfolders(string const &folder_name, string const &file_extension = "");
 
-	//
+	/// <summary>
+	/// This function returns current date and time.
+	/// </summary>
+	/// <param name="format">This parameter, which supports strings, can be customized by using the following variables (no specified requirement):
+	/// %Y = Current year
+	/// %m = Current month
+	/// %d = Current day
+	/// %H = Current hour
+	/// %M = Current minute
+	/// %S = Current seconds
+	/// E.g. => %d\/%m\/%Y	01/01/1980.</param>
 	string CurrentDateTime(const char* format);
 
-	// 
+	/// <summary>
+	/// This boolean function checks if a precise folder exists in a specified folder path.
+	/// </summary>
+	/// <param name="folderPath">Folder path. Supports strings.</param>
 	bool CheckIfFolderExists(string folderPath);
 
+	/// <summary>
+	/// This function removes a file into a specified path.
+	/// </summary>
+	/// <param name="filePath">File path. Supports strings.</param>
 	void RemoveFile(string filePath);
 
+	/// <summary>
+	/// This function creates a folder into a specified path.
+	/// </summary>
+	/// <param name="folderPth">Folder path. Supports strings.</param>
 	void CreateFolder(string folderPath);
 
+	/// <summary>
+	/// This function returns the relative file folder path.
+	/// </summary>
+	/// <param name="*path">Folder path. Supports only referenced char variables or strings converted to c_str.</param>
 	string GetFileFolderPath(const char *path);
 };
 
--- centurion-source/source/util/maths.h
+++ centurion-source/source/util/maths.h
@@ -1,4 +1,12 @@
-﻿#pragma once
+﻿/*
+* ---------------------------
+* CENTURION
+* [2019] - [2020] Rattlesmake
+* All Rights Reserved.
+* ---------------------------
+*/
+
+#pragma once
 
 #include<array>
 
@@ -6,14 +14,36 @@
 
 using namespace std;
 
+/// <summary>
+/// This namespace contains some mathematical functions.
+/// </summary>
 namespace math {
 	
-	//
+	/// <summary>
+	/// This function calculates the euclidean distance between two points
+	/// </summary>
+	/// <param name="x1">X coordinate of the first point</param>
+	/// <param name="y1">Y coordinate of the first point</param>
+	/// <param name="x2">X coordinate of the second point</param>
+	/// <param name="y2">Y coordinate of the second point</param>
 	float euclidean_distance(float x1, float y1, float x2, float y2);
 	
-	//
+	/// <summary>
+	/// This function calculates the distance between two points within an ellipse
+	/// </summary>
+	/// <param name="x">X coordinate of the first point</param>
+	/// <param name="y">Y coordinate of the first point</param>
+	/// <param name="cx">X coordinate of the second point</param>
+	/// <param name="cy">Y coordinate of the second point</param>
+	/// <param name="r">Ellipse radius</param>
 	float ellipse_distance(float x, float y, float cx, float cy, float r);
 	
-	// function for empty rectangle creation
+	/// <summary>
+	/// This function creates a rectangle into a specified point
+	/// </summary>
+	/// <param name="x0">X coordinate of the rectangle</param>
+	/// <param name="y0">Y coordinate of the rectangle</param>
+	/// <param name="w">Rectangle width</param>
+	/// <param name="h">Rectangle height</param>
 	array<float, 8> get_rectangle_coords(float x0, float y0, float w, float h);
 };
