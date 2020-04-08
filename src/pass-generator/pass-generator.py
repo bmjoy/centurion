5723053531f4689b379ed640798eae83eeeeb28c
Fabio Marigo <39345385+fabiomarigo7@users.noreply.github.com>
2020-04-08 16:53:19 +0200

    [Pass] {Pass.txt}

--- src/pass-generator/pass-generator.py
+++ src/pass-generator/pass-generator.py
@@ -3,7 +3,6 @@
 # [2019] - [2020] Rattlesmake
 # All Rights Reserved.
 # ---------------------------
-
 from PIL import Image
 import numpy as np
 import os
@@ -28,8 +27,8 @@ def passCreate():
                     width = data.shape[1]
                     height = data.shape[0]
                     
-                    xSize = width//GRID_CELL_SIZE//4 * 4 + 4 * int(width//GRID_CELL_SIZE%4 != 0)
-                    ySize = height//GRID_CELL_SIZE//4 * 4 + 4 * int(height//GRID_CELL_SIZE%4 != 0)
+                    xSize = width // GRID_CELL_SIZE // 4 * 4 + 4 * int(width // GRID_CELL_SIZE % 4 != 0)
+                    ySize = height // GRID_CELL_SIZE // 4 * 4 + 4 * int(height // GRID_CELL_SIZE % 4 != 0)
                                        
                     out_data = []
                     for iy in range(ySize): 
@@ -38,13 +37,13 @@ def passCreate():
                             l1 += [0]
                         out_data += [l1]
                         
-                    for iy in range(round(height/GRID_CELL_SIZE)): 
-                        for ix in range(round(width/GRID_CELL_SIZE)):
-                            value = int(data[iy*GRID_CELL_SIZE, ix*GRID_CELL_SIZE]/255)                            
+                    for iy in range(round(height / GRID_CELL_SIZE)): 
+                        for ix in range(round(width / GRID_CELL_SIZE)):
+                            value = int(data[iy * GRID_CELL_SIZE, ix * GRID_CELL_SIZE] / 255)                            
                             if (value == 1):
                                 out_data[iy][ix] = 1
                     
-                    f = open(name+".xml", "w")
+                    f = open(name + ".xml", "w")
                     
                     f.write("<!-- Centurion -->\n")
                     f.write("<!-- [2019] - [2020] Rattlesmake -->\n")
@@ -74,6 +73,6 @@ def passCreate():
     except Exception as e: 
         input(e)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     passCreate()
     input('Press Enter to close.')
